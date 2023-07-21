ba0b99057799438e181af519f83b00bcce1710de
Navid Sassan <navid@sassan.ch>
2023-07-21 13:26:25 +0200

    finished rfid_lock_advanced

---
+++ rfid_lock_advanced.md
@@ -0,0 +1,126 @@
+# Puzzle 1 (gray)
+## Level 1
+    1: Grün
+    2: Grau
+    3: Blau
+    4: Gelb
+
+## Level 2
+### Riddle square
+    1: Grün
+    2: Blau
+    3: Rot
+    4: Gelb
+
+### Riddle square-circle
+    1: Blau
+    2: Grau
+    3: Gelb
+    4: Rot
+
+### Riddle square-triangle
+    1: Rot
+    2: Grün
+    3: Grau
+    4: Gelb
+
+### Riddle circle
+    1: Gelb
+    2: Grau
+    3: Blau
+    4: Grün
+
+### Riddle circle-square
+    1: Grau
+    2: Gelb
+    3: Grün
+    4: Rot
+
+### Riddle circle-triangle
+    1: Grün
+    2: Rot
+    3: Gelb
+    4: Grau
+
+### Riddle triangle
+    1: Blau
+    2: Rot
+    3: Grau
+    4: Grün
+
+### Riddle triangle-square
+    1: Gelb
+    2: Grün
+    3: Rot
+    4: Blau
+
+### Riddle triangle-circle
+    1: Rot
+    2: Gelb
+    3: Blau
+    4: Grau
+
+
+# Puzzle 2 (black)
+## Level 1
+    1: Blau
+    2: Orange
+    3: Rot
+    4: Schwarz
+
+## Level 2
+### Riddle square
+    1: Blau
+    2: Rot
+    3: Gelb
+    4: Schwarz
+
+### Riddle square-circle
+    1: Rot
+    2: Orange
+    3: Schwarz
+    4: Gelb
+
+### Riddle square-triangle
+    1: Gelb
+    2: Blau
+    3: Orange
+    4: Schwarz
+
+### Riddle circle
+    1: Schwarz
+    2: Orange
+    3: Rot
+    4: Blau
+
+### Riddle circle-square
+    1: Orange
+    2: Schwarz
+    3: Blau
+    4: Gelb
+
+### Riddle circle-triangle
+    1: Blau
+    2: Gelb
+    3: Schwarz
+    4: Orange
+
+### Riddle triangle
+    1: Rot
+    2: Gelb
+    3: Orange
+    4: Blau
+
+### Riddle triangle-square
+    1: Schwarz
+    2: Blau
+    3: Gelb
+    4: Rot
+
+### Riddle triangle-circle
+    1: Gelb
+    2: Schwarz
+    3: Rot
+    4: Orange
+
+
--- rfid_lock_advanced.py
+++ rfid_lock_advanced.py
@@ -1,9 +1,13 @@
 # https://electropeak.com/learn/interfacing-rdm6300-125khz-rfid-reader-module-with-arduino/
+# https://docs.micropython.org/en/latest/library/framebuf.html
+
+import functools
+import random
+import time
+from array import array
 
 import ssd1306
-import time
-from machine import Pin, SoftI2C, UART
-
+from machine import UART, Pin, SoftI2C
 
 # pinout:
 # 25 display sda
@@ -15,6 +19,7 @@ from machine import Pin, SoftI2C, UART
 
 class Puzzle():
 
+
     def __init__(self):
         i2c = SoftI2C(sda=Pin(25), scl=Pin(33))
         self.display = ssd1306.SSD1306_I2C(128, 64, i2c)
@@ -24,6 +29,161 @@ class Puzzle():
 
         self.btn_input = Pin(26, Pin.IN, Pin.PULL_UP)
         self.btn_led = Pin(32, Pin.OUT)
+
+        self.card2color = {
+            12906581: 'Grün',
+            13012325: 'Grau',
+            1338145: 'Schwarz',
+            1484985: 'Gelb',
+            15798243: 'Orange',
+            6008435: 'Gelb',
+            7888819: 'Blau',
+            8202175: 'Blau',
+            8644032: 'Rot',
+            8736126: 'Rot',
+        }
+
+        self.card2sol_level1 = {
+            #     puzzle1,  puzzle2,   # puzzle1,  puzzle2,
+            'A': [13012325, 15798243], # grey,     orange
+            'B': [6008435,  1338145],  # yellow,   black
+            'C': [8644032,  1484985],  # red,      yellow
+            'D': [8202175,  8736126],  # blue,     red
+            'E': [12906581, 7888819],  # green,    blue
+        }
+
+        self.solution_level1 = [
+            'E',
+            'A',
+            'D',
+            'B',
+        ]
+
+        self.card2sol_level2 = {
+            #     puzzle1,  puzzle2,   # puzzle1,  puzzle2,
+            'A': [12906581, 7888819],  # green,    blue
+            'B': [13012325, 15798243], # grey,     orange
+            'C': [6008435,  1338145],  # yellow,   black
+            'D': [8202175,  8736126],  # blue,     red
+            'E': [8644032,  1484985],  # red,      yellow
+        }
+
+        self.riddles_level2 = [
+            {
+                'name': 'square',
+                'display_functions': [
+                    functools.partial(self.display.rect, 50, 30, 30, 30, 1, True),
+                ],
+                'solution': [
+                    'A',
+                    'D',
+                    'E',
+                    'C',
+                ],
+            },
+            {
+                'name': 'square-circle',
+                'display_functions': [
+                    functools.partial(self.display.rect, 30, 30, 30, 30, 1, True),
+                    functools.partial(self.display.ellipse, 80, 45, 15, 15, 1, True),
+                ],
+                'solution': [
+                    'D',
+                    'B',
+                    'C',
+                    'E',
+                ],
+            },
+            {
+                'name': 'square-triangle',
+                'display_functions': [
+                    functools.partial(self.display.rect, 25, 30, 30, 30, 1, True),
+                    functools.partial(self.display.poly, 65, 9, array('h', [0, 50, 15, 30, 30, 50]), 1, True),
+                ],
+                'solution': [
+                    'E',
+                    'A',
+                    'B',
+                    'C',
+                ],
+            },
+            {
+                'name': 'circle',
+                'display_functions': [
+                    functools.partial(self.display.ellipse, 60, 40, 15, 15, 1, True),
+                ],
+                'solution': [
+                    'C',
+                    'B',
+                    'D',
+                    'A',
+                ],
+            },
+            {
+                'name': 'circle-square',
+                'display_functions': [
+                    functools.partial(self.display.ellipse, 40, 45, 15, 15, 1, True),
+                    functools.partial(self.display.rect, 60, 30, 30, 30, 1, True),
+                ],
+                'solution': [
+                    'B',
+                    'C',
+                    'A',
+                    'E',
+                ],
+            },
+            {
+                'name': 'circle-triangle',
+                'display_functions': [
+                    functools.partial(self.display.ellipse, 40, 45, 15, 15, 1, True),
+                    functools.partial(self.display.poly, 65, 9, array('h', [0, 50, 15, 30, 30, 50]), 1, True),
+                ],
+                'solution': [
+                    'A',
+                    'E',
+                    'C',
+                    'B',
+                ],
+            },
+            {
+                'name': 'triangle',
+                'display_functions': [
+                    functools.partial(self.display.poly, 45, 0, array('h', [0, 50, 15, 30, 30, 50]), 1, True),
+                ],
+                'solution': [
+                    'D',
+                    'E',
+                    'B',
+                    'A',
+                ],
+            },
+            {
+                'name': 'triangle-square',
+                'display_functions': [
+                    functools.partial(self.display.poly, 25, 9, array('h', [0, 50, 15, 30, 30, 50]), 1, True),
+                    functools.partial(self.display.rect, 70, 30, 30, 30, 1, True),
+                ],
+                'solution': [
+                    'C',
+                    'A',
+                    'E',
+                    'D',
+                ],
+            },
+            {
+                'name': 'triangle-circle',
+                'display_functions': [
+                    functools.partial(self.display.poly, 25, 9, array('h', [0, 50, 15, 30, 30, 50]), 1, True),
+                    functools.partial(self.display.ellipse, 80, 45, 15, 15, 1, True),
+                ],
+                'solution': [
+                    'E',
+                    'C',
+                    'D',
+                    'B',
+                ],
+            },
+        ]
 
 
     def read_card(self):
@@ -56,26 +216,38 @@ class Puzzle():
             card_id = card_data[2:].decode('ascii')
 
             # convert hex to decimal
-            return(int(card_id, 16))
+            return int(card_id, 16)
+        return None
+
+
+    def check_solution(self, solution, given_cards, card2sol):
+        print('check_solution')
+        for sol, given_card in zip(solution, given_cards):
+            print(f'\tsol: {sol}')
+            print(f'\tcard2sol[sol]: {card2sol[sol]} ({", ".join(self.card2color[card] for card in card2sol[sol])})')
+            print(f'\tgiven_card: {given_card} ({self.card2color[given_card]})')
+            if given_card not in card2sol[sol]:
+                print('\tsolution incorrect')
+                return False
+        print('\tsolution correct')
+        return True
 
 
     def run(self):
         self.level1()
         self.level2()
 
+        self.display.fill(0)
+        self.display.text('Code:', 0, 0, 1)
+        self.display.text('1234 oder 1234', 0, 50, 1)
+        self.display.show()
+
 
     def level1(self):
         # List of card IDs for unlocking
-        correct_card_ids = [
-            8202175, # 0: blue
-            12906581, # 1: green
-            13012325, # 2: grey
-            6008435, # 3: yellow
-        ]
-
         # repeat until solved, then exit the method
         while True:
-            seen_card_ids = []
+            read_card_ids = []
             current_card_index = 0
 
             # wait until 4 cards are read, then break and check the card IDs
@@ -88,14 +260,15 @@ class Puzzle():
 
                 read_card_id = self.read_card()
 
-                if read_card_id is not None and read_card_id not in seen_card_ids:
+                if read_card_id is not None and read_card_id not in read_card_ids:
                     # blink button LED
                     self.btn_led.on()
                     time.sleep(0.2)
                     self.btn_led.off()
 
+                    print(f'read_card_id: {read_card_id} ({self.card2color[read_card_id]})')
                     current_card_index += 1
-                    seen_card_ids.append(read_card_id)
+                    read_card_ids.append(read_card_id)
 
                     if current_card_index == 4:
                         print('got 4 cards, wait for button press now')
@@ -115,7 +288,7 @@ class Puzzle():
                 self.btn_led.off()
                 time.sleep(0.2)
 
-            if seen_card_ids == correct_card_ids:
+            if self.check_solution(self.solution_level1, read_card_ids, self.card2sol_level1):
                 self.display.fill(0)
                 self.display.text('Level 1', 0, 0, 1)
                 self.display.text(f'{"X" * current_card_index:?<4}', 0, 20, 1)
@@ -133,16 +306,111 @@ class Puzzle():
                 time.sleep(2) # sleep so one can read the messages on the screen
                 print('wrong, therefore repeat')
 
+                # wait until read_card returns None, else we get a wrong first read
+                while (temp := self.read_card()) is not None:
+                    print(f'temp: {temp}')
+                    time.sleep(0.1)
+
 
     def level2(self):
-        self.display.fill(0)
-        self.display.text('Level 2', 0, 0, 1)
-        self.display.text('????', 0, 20, 1)
-        self.display.show()
+        # repeat until solved, then exit the method
+        while True:
+            current_riddle = random.choice(self.riddles_level2)
+            read_card_ids = []
+            current_card_index = 0
 
-        # while True:
-        #     pass
+            # wait until 4 cards are read, then break and check the card IDs
+            while True:
+                print(f'current_card_index: {current_card_index}')
+                self.display.fill(0)
+                self.display.text('Level 2', 0, 0, 1)
+                self.display.text(f'{"X" * current_card_index:?<4}', 0, 20, 1)
+                for display_function in current_riddle['display_functions']:
+                    display_function()
+                self.display.show()
+
+                read_card_id = self.read_card()
+
+                if read_card_id is not None and read_card_id not in read_card_ids:
+                    # blink button LED
+                    self.btn_led.on()
+                    time.sleep(0.2)
+                    self.btn_led.off()
+
+                    print(f'read_card_id: {read_card_id} ({self.card2color[read_card_id]})')
+                    current_card_index += 1
+                    read_card_ids.append(read_card_id)
+
+                    if current_card_index == 4:
+                        print('got 4 cards, wait for button press now')
+                        break
+
+                time.sleep(0.2)
+
+            self.display.fill(0)
+            self.display.text('Level 2', 0, 0, 1)
+            self.display.text(f'{"X" * current_card_index:?<4}', 0, 20, 1)
+            for display_function in current_riddle['display_functions']:
+                display_function()
+            self.display.show()
+
+            # wait for button press
+            while self.btn_input.value() == 1:
+                self.btn_led.on()
+                time.sleep(0.2)
+                self.btn_led.off()
+                time.sleep(0.2)
+
+            if self.check_solution(current_riddle['solution'], read_card_ids, self.card2sol_level2):
+                self.display.fill(0)
+                self.display.text('Level 2', 0, 0, 1)
+                self.display.text(f'{"X" * current_card_index:?<4}', 0, 20, 1)
+                self.display.text('SOLVED!', 0, 40, 1)
+                self.display.show()
+                time.sleep(2) # sleep so one can read the messages on the screen
+                return # exit the function
+
+            else:
+                self.display.fill(0)
+                self.display.text('Level 2', 0, 0, 1)
+                self.display.text(f'{"X" * current_card_index:?<4}', 0, 20, 1)
+                self.display.text('WRONG!', 0, 40, 1)
+                self.display.show()
+                time.sleep(2) # sleep so one can read the messages on the screen
+                print('wrong, therefore repeat')
+
+                # wait until read_card returns None, else we get a wrong first read
+                while (temp := self.read_card()) is not None:
+                    print(f'temp: {temp}')
+                    time.sleep(0.1)
+
+
+    def print_solutions(self):
+        for puzzle_index, puzzle_name in enumerate(['gray', 'black']):
+            print(f'# Puzzle {puzzle_index + 1} ({puzzle_name})')
+
+            print('## Level 1')
+            for i, sol in enumerate(self.solution_level1):
+                card = self.card2sol_level1[sol][puzzle_index]
+                print(f'\t{i + 1}: {self.card2color[card]}')
+            print()
+
+            print('## Level 2')
+            for riddle in self.riddles_level2:
+                print(f'### Riddle {riddle["name"]}')
+                for i, sol in enumerate(riddle['solution']):
+                    card = self.card2sol_level2[sol][puzzle_index]
+                    print(f'\t{i + 1}: {self.card2color[card]}')
+                print()
+            print()
+
 
 if __name__ == '__main__':
     puzzle = Puzzle()
     puzzle.run()
+
+    # puzzle.print_solutions()
+
+    # while True:
+    #     print(puzzle.read_card())
+    #     time.sleep(0.2)
