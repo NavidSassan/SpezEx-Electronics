e0d778fbc02d43a83d4f7053e3c9aa66a09f1756
Navid Sassan <navid@sassan.ch>
2023-07-28 12:06:07 +0200

    adjust speaker code

--- speaker.py
+++ speaker.py
@@ -3,29 +3,14 @@
 import time
 from machine import Pin, PWM
 
-speaker = PWM(Pin(35))
+speaker = PWM(Pin(32))
 
-speaker.duty_u16(1000)
-speaker.freq(300) # 1 Kilohertz
-
-time.sleep(.5) # wait a 1/4 second
-
-speaker.duty_u16(0)
-time.sleep(.25)
+speaker.duty_u16(60000)
 
 
-speaker.duty_u16(1000)
-speaker.freq(800)
+while True:
+    speaker.freq(100)
+    time.sleep(1)
 
-time.sleep(.5)
-
-speaker.duty_u16(0)
-time.sleep(.25)
-
-
-speaker.duty_u16(1000)
-speaker.freq(400)
-time.sleep(.5)
-
-# turn off the PWM
-speaker.duty_u16(0)
+    speaker.freq(300)
+    time.sleep(1)
